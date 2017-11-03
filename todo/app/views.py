4d11969d20c4ab9d1fb6a61c43c484af333f3d1d
neelansh <neelansh5@gmail.com>
2017-11-03 23:12:10 +0530

    todo api created

---
+++ todo/app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Task
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import CreateAPIView
+from .serializer import TaskSerializer, UserSerializer
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from django.contrib.auth import get_user_model
+
+# Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+	permission_classes = (IsAuthenticated, ) 
+	model = Task;
+	serializer_class = TaskSerializer;
+	filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter, )
+	filter_fields = ('completed', )
+	ordering = ('-date_created', )
+	search_fields = ('task_name', )
+
+	def get_queryset(self):
+		queryset = self.model.objects.all().filter(user = self.request.user);
+		return queryset
+
+	def perform_create(self, serializer):
+		return serializer.save(user = self.request.user);
+
+
+
+
+class CreateUserView(CreateAPIView):
+	model = get_user_model();
+	permission_classes = (AllowAny, )
+	serializer_class = UserSerializer
